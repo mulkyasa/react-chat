5a41cabde01bdaaa6eec546db123b3dca7b5e4ce
mulkyasa <mulkyasa@gmail.com>
2020-04-05 13:26:03 +0700

    adding coket.io on server

--- server/app.js
+++ server/app.js
@@ -4,11 +4,32 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var cors = require('cors');
+var io = require('socket.io')(http);
+
+/**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('react-chat:server');
+var http = require('http');
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
 
 var indexRouter = require('./routes/index');
 var chatsRouter = require('./routes/chats');
 
 var app = express();
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 mongoose.connect('mongodb://localhost:27017/chatdb', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -22,4 +43,86 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/api/chats', chatsRouter);
 
-module.exports = app;
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+
+  socket.on('add chat', (chatData = {}) => {
+    socket.broadcast.emit('load chat', chatData);
+  });
+
+  socket.on('delete chat', id => {
+    socket.broadcast.emit('delete chat', id);
+  })
+
+})
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
